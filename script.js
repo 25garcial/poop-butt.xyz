8883799ae2b246ec6a38e46ab4e9e40901ed1d3e
25garcial <91680716+25garcial@users.noreply.github.com>
2024-12-10 10:15:38 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function main(){
+function playChess(){
+let doc=document.createElement("script")
+doc.src="https://raw.githubusercontent.com/Logan-Garcia-inc/browser-Chess/refs/heads/main/script.js";
+	document.body.append(doc)
+  console.log("sdd")
 }
-main()
+document.querySelector(".chessButton").addEventListener("click",playChess)
