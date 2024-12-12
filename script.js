ec1c4101a71380cd9d0abb220c9646317f0b4644
25garcial <91680716+25garcial@users.noreply.github.com>
2024-12-12 13:35:53 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-async function playChess(){
-let doc=document.createElement("script")
-let script=await fetch("https://raw.githubusercontent.com/Logan-Garcia-inc/browser-Chess/refs/heads/main/script.js");
-script=await script.text()
-doc.textContent=script
-	document.body.append(doc)
+async function playChess() {
+  let doc = document.createElement("script")
+  let script = await fetch(
+    "https://raw.githubusercontent.com/Logan-Garcia-inc/browser-Chess/refs/heads/main/script.js",
+  )
+  script = await script.text()
+  doc.textContent = script
+  document.body.append(doc)
 }
+document.querySelector(".chessButton").addEventListener("click", () => {
+  playChess()
+})
 
-document.querySelector(".chessButton").addEventListener("click",()=>{
-playChess()})
+for (let i of document.querySelectorAll("img")) {i.src = "https://github.com/25garcial/poop-butt.xyz/blob/main/" +i.src.slice("https://fiddle.jshell.net/_display".length,i.src.length)+"?raw=true"}
+
